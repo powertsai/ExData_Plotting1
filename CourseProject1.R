ac4a8357d932ca51130f0e17e4064821242b3993
AllenMLTsai <powertsai@gmail.com>
2015-08-09 16:39:20 +0800

    update R and png

--- CourseProject1.R
+++ CourseProject1.R
@@ -18,10 +18,6 @@ getData <- function(fileName = fileName,...) {
   #skip 63000 rows and read 20000 rows only
   household_data <- read.table(file=fileName,...)
   names(household_data) <- names(household_data_first100)
-  # require library dplyr to use mutate, filter to get date in 2007/02/01, 2007/02/02
-  library(dplyr)
-  # require lubridate to get year, month, mday to filter data
-  library(lubridate)
   subset_household_data <-
     household_data %>% 
     mutate(datetime = dmy_hms(paste(Date, Time) ) ) %>%
@@ -29,7 +25,7 @@ getData <- function(fileName = fileName,...) {
     mutate(year = year(Date)) %>%
     mutate(month = month(Date)) %>%
     mutate(mday = mday(Date)) %>%
-    mutate(wday = wday(Date, label = TRUE)) %>%
+    mutate(wday = substr(wday(Date, label = TRUE),1,3)) %>%
     mutate(hour = hour(datetime)) %>%
     mutate(minute = minute(datetime)) %>%
     mutate(second = second(datetime)) %>%
@@ -44,7 +40,7 @@ setXAxis <- function(subset_household_data){
   #pick up datetime at 00:00:00 to place x axis label
   axisSubSet = filter(subset_household_data, hour == 0 & minute == 0 & second == 0)
   #Add X-Axis to xyPlot
-  with(axisSubSet, axis(1, at=datetime,labels=wday, col.axis="black", las=2) )
+  with(axisSubSet, axis(1, at=datetime,labels=wday, col.axis="black") )
 }
 
 
